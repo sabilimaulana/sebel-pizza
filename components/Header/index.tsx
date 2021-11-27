6fdb90d5107c3182140d28f8a194edc6d2052b54
sabilimaulana <spacysbl.learn@gmail.com>
2021-11-27 21:31:05 +0700

    Animating SVG's

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,25 +1,53 @@
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { NextComponentType } from "next";
 import Link from "next/link";
 import { NextRouter, useRouter } from "next/dist/client/router";
 
-const Header: NextComponentType = () => {
-  const router: NextRouter = useRouter();
+const svgVariants: Variants = {
+  hidden: { rotate: -180 },
+  visible: {
+    rotate: 0,
+    transition: {
+      duration: 1
+    }
+  }
+};
 
+const pathVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: { duration: 2, ease: "easeInOut" }
+  }
+};
+
+const Header: NextComponentType = () => {
   return (
     <header>
       <Link href="/" passHref>
         <div className="logo">
-          <svg
+          <motion.svg
             className="pizza-svg"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100">
-            <path
+            viewBox="0 0 100 100"
+            variants={svgVariants}
+            initial="hidden"
+            animate="visible">
+            <motion.path
               fill="none"
               d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+              variants={pathVariants}
             />
-            <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-          </svg>
+            <motion.path
+              fill="none"
+              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+              variants={pathVariants}
+            />
+          </motion.svg>
         </div>
       </Link>
 
